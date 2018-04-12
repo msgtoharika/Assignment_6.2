34093f7598f6e9ebfdfdb95d125e66aa3e615f15
msgtoharika <36808169+msgtoharika@users.noreply.github.com>
2018-04-12 23:17:30 +0530

    Create recipe.js

---
+++ recipe.js
@@ -0,0 +1,29 @@
+//Declaring and initialising variables
+var recipeName = 'Creamy squash linguine';
+var preparationTime = 45;
+var recipeBy = 'Jimmy cutter';
+var isVegetarian = true;
+var calories = 223.7;
+
+
+//Printing the variables in specified format
+console.log("Recipe details");
+console.log("------------------------");
+console.log("Recipe Name: " +recipeName);
+console.log("Preparation Time: " +preparationTime);
+console.log("Recipe By: " +recipeBy);
+console.log("is Vegetarian Dish: " +isVegetarian);
+console.log("Calories: " +calories);
+
+//Re-initialsing recipeBy and preparationTime 
+var recipeBy = 'Jimmy Cutter George';
+var preparationTime = 60;
+
+//Printing the variables after updating preparation time and recipeBy
+console.log("Recipe details after update");
+console.log("------------------------");
+console.log("Recipe Name: " +recipeName);
+console.log("Preparation Time: " +preparationTime);
+console.log("Recipe By: " +recipeBy);
+console.log("is Vegetarian Dish: " +isVegetarian);
+console.log("Calories: " +calories);
